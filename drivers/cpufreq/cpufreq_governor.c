3a13cb52c32837525a0c3fbea5a55beb1b8fcf61
eoghan2t9 <eoghan61@gmail.com>
2016-01-04 00:29:02 +0000

    Linux 3.10.37
    Conflicts:
    	buildzf2

--- drivers/cpufreq/cpufreq_governor.c
+++ drivers/cpufreq/cpufreq_governor.c
@@ -222,6 +222,16 @@ void gov_add_timers(struct cpufreq_policy *policy, unsigned int delay)
 		cdbs = dbs_data->cdata->get_cpu_cdbs(cpu);
 		cdbs->timer.expires = jiffies + delay;
 		add_timer_on(&cdbs->timer, cpu);
+	int i;
+
+	if (!policy->governor_enabled)
+		return;
+
+	if (!all_cpus) {
+		__gov_queue_work(smp_processor_id(), dbs_data, delay);
+	} else {
+		for_each_cpu(i, policy->cpus)
+			__gov_queue_work(i, dbs_data, delay);
 	}
 }
 EXPORT_SYMBOL_GPL(gov_add_timers);
