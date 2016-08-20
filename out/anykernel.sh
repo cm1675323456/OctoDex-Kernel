2b4e05ee7c536846e515fc3d8a7f0843886c614d
Nyks45 <winglow45@gmail.com>
2016-08-20 18:21:37 +0300

    [WIP]Re-Try To Update Packer

--- out/anykernel.sh
+++ out/anykernel.sh
@@ -1,9 +1,9 @@
-# AnyKernel 2.0 Ramdisk Mod Script 
+# AnyKernel2 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
 ## AnyKernel setup
 # EDIFY properties
-kernel.string=OctoDex Kernel by Nyks45
+kernel.string=OctoDex by Nyks45 @ xda-developers
 do.devicecheck=0
 do.initd=1
 do.modules=0
@@ -29,10 +29,12 @@ patch=/tmp/anykernel/patch;
 
 chmod -R 777 $bin;
 mkdir -p $ramdisk $split_img;
-#cd /tmp/anykernel;
 
 OUTFD=`ps | grep -v "grep" | grep -oE "update(.*)" | cut -d" " -f3`;
 ui_print() { echo "ui_print $1" >&$OUTFD; echo "ui_print" >&$OUTFD; }
+
+# contains <string> <substring>
+contains() { test "${1#*$2}" != "$1" && return 0 || return 1; }
 
 # dump boot and extract ramdisk
 dump_boot() {
@@ -46,14 +48,12 @@ dump_boot() {
 }
 
 # repack ramdisk then build and write image
-write_boot() {
-  /tmp/anykernel/tools/mkbootimg --kernel /tmp/anykernel/bzImage --ramdisk /tmp/anykernel/initramfs.cpio.gz --cmdline "init=/init pci=noearly loglevel=0 vmalloc=256M androidboot.hardware=mofd_v1 watchdog.watchdog_thresh=60 androidboot.spid=xxxx:xxxx:xxxx:xxxx:xxxx:xxxx androidboot.serialno=01234567890123456789 snd_pcm.maximum_substreams=8 ip=50.0.0.2:50.0.0.1::255.255.255.0::usb0:on debug_locks=0 bootboost=1 androidboot.selinux=permissive" --base 0x10000000 --pagesize 2048 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --second /tmp/anykernel/second.gz -o /tmp/anykernel/boot-new.img;
-
-  if [ $? != 0 -o `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
+ /tmp/anykernel/tools/mkbootimg --kernel /tmp/anykernel/bzImage --ramdisk /tmp/anykernel/initramfs.cpio.gz --cmdline "init=/init pci=noearly loglevel=0 vmalloc=256M androidboot.hardware=mofd_v1 watchdog.watchdog_thresh=60 androidboot.spid=xxxx:xxxx:xxxx:xxxx:xxxx:xxxx androidboot.serialno=01234567890123456789 snd_pcm.maximum_substreams=8 ip=50.0.0.2:50.0.0.1::255.255.255.0::usb0:on debug_locks=0 bootboost=1 androidboot.selinux=permissive" --base 0x10000000 --pagesize 2048 --ramdisk_offset 0x01000000 --tags_offset 0x00000100 --second /tmp/anykernel/second.gz -o /tmp/anykernel/boot-new.img;
+if [ $? != 0 -o `wc -c < /tmp/anykernel/boot-new.img` -gt `wc -c < /tmp/anykernel/boot.img` ]; then
     ui_print "Repacking image failed. Aborting...";
     echo "Repack Failed" > /tmp/anykernel/exitcode; exit;
-  fi;
-  dd if=/tmp/anykernel/boot-new.img of=$block;
+fi;
+dd if=/tmp/anykernel/boot-new.img of=$block;
 }
 
 ## end methods
