f3fbd49b7e5315abfc66adfb48d6197e768c6ca8
Nyks45 <winglow45@gmail.com>
2016-10-03 22:07:04 +0200

    Revert "[REVERTME]EXT4: use e/frandom"
    This reverts commit 7c76f6f03daf0485734108de32c5cbd06f054c1b.

--- fs/ext4/mmp.c
+++ fs/ext4/mmp.c
@@ -257,11 +257,7 @@ static unsigned int mmp_new_seq(void)
 	u32 new_seq;
 
 	do {
-<<<<<<< HEAD
 		new_seq = prandom_u32();
-=======
-		erandom_get_random_bytes((char *)&new_seq, (size_t)sizeof(u32));
->>>>>>> cf39d06... EXT4: use e/frandom
 	} while (new_seq > EXT4_MMP_SEQ_MAX);
 
 	return new_seq;
